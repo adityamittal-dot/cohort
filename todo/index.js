e930c97382cc8348e2e50e8ce000b97c0515007b
adityamittal-dot <adityamittal529@gmail.com>
2026-04-22 12:06:15 +0530

    feat(todo): add auth middleware and DB connection setup

--- todo/index.js
+++ todo/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const {UserModel, TodoModel} = require("./db");
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = "adityamittal@gmail.com";
+const mongoose = require("mongoose");
+
+mongoose.connect("//put localDb path here for postman connection");
+
 const app = express();
 app.use(express.json());
 
@@ -53,5 +57,20 @@ app.post("/todo", function(req, res){
 app.get("/todos", function(req, res){
   
 });
+
+function auth(req, res, next){
+  const token =  req.header.token
+  const decodeData = jwt.verify(token, JWT_SECRET);
+
+  if(decodeData){
+    req.userId = decodedData.userId;
+    next();
+
+  } else {
+    res.status(403).json({
+      message: "invalid credentials"
+    })
+  } 
+}
 
 app.listen(3000);
