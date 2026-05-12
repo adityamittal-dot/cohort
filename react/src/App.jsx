d93d1c1faac9dbfaaf2882df23ff494d9012b488
adityamittal-dot <adityamittal529@gmail.com>
2026-05-13 02:06:04 +0530

    update App.jsx and add post.jsx

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,24 +1,35 @@
-function app(){
-  return(
-    <div>
-      <h1>hello world</h1>
+import { postComponent } from "./post";
+
+function App(){
+
+  const[post, setPost] = useState([]);
+
+  const postComponent = post.map(post => <postComponent
+    name={post.name}
+    subtitle={post.subtitle}
+    time={post.time}
+    image={post.image}
+    description={post.description}
+  />)
+
+  // originally it was array of objects then we converted it to an array of objects and passed it into postComponents
+  //this is how to render a bunch of array of objects to seperate components
+
+  function addPost(){
+
+  }
+
+  return (
+    <div style={{background: '#dfe6e9', height: "100vh"}}>
+      <button onClick={addPost}> ADD POST </button>
+      <div style={{display: "flex", justifyContent: "center"}}>
+        <div>
+          {postComponent}
+        </div>
+      </div>
     </div>
   )
+
 }
 
-const toggleMessage = () => {
-  let [isVisible, setIsVisible] = useState(true);
-
-  function handleToggle(){
-    setIsVisible(!isVisible);
-  }
-  return ( <div>
-    <button onClick={handleToggle}>Toggle</button>
-    {isVisible ? <p>Hello World</p> : null}
-  </div>
-  );
-
-  
-}
-
-export default app; 
+export default App;
---
+++ react/src/post.jsx
@@ -0,0 +1,27 @@
+const style = { width: 200, backgroundColor:"#252525", borderRadius: 10, borderColor: "gray", borderWidth: 1, padding: 20 }
+
+export function postComponent({name, subtitle, time, image, description}) {
+  return <div style={style}>
+    <div style ={{display: 'flex', alingItem: 'center'}}>
+      <img src = {image} style={{
+        width: 30,
+        height: 30,
+        borderRadius: 25
+      }} />
+      <div style={{fontSize: 10, marginLeft: 10}}>
+        <b>
+          {name}
+        </b>
+        <div>{subtitle}</div>
+        {(time !== undefined) ? <div style={{display: 'flex'}}>
+          <div>{time}</div>
+          <img src={""} style={{width: 12, height: 12}}/>
+        </div> : null }
+      </div>
+    </div>
+    <div style={{fontSize: 12}}>
+      {description}
+    </div>
+  </div>
+
+}
