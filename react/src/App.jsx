a613ef384c2e13a08b38fd67a0af4b5987866f36
adityamittal-dot <adityamittal529@gmail.com>
2026-05-15 22:05:37 +0530

    Refactor App.jsx to use simple counter with useEffect

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,45 +1,63 @@
-import { useState } from "react";
-import { PostComponent } from "./post";
+// import { useState } from "react";
+// import { PostComponent } from "./post";
+
+// function App(){
+
+//   const[post, setPost] = useState([]);
+
+//   const postComponents = post.map((p, index) => <PostComponent
+//     key={index}
+//     name={p.name}
+//     subtitle={p.subtitle}
+//     time={p.time}
+//     image={p.image}
+//     description={p.description}
+//   />)
+
+//   // originally it was array of objects then we converted it to an array of objects and passed it into postComponents
+//   //this is how to render a bunch of array of objects to seperate components
+
+//   function addPost(){
+//     setPost([...post, {
+//       name: "aditya",
+//       subtitle: "how to grow",
+//       time: "2m ago",
+//       image: "",
+//       description: "howwwwwwww tooooooooo growwwwwww"
+//     }
+
+//     ])
+//   }
+
+//   return (
+//     <div style={{background: '#dfe6e9', height: "100vh"}}>
+//       <button onClick={addPost}> ADD POST </button>
+//       <div style={{display: "flex", justifyContent: "center"}}>
+//         <div>
+//           {postComponents}
+//         </div>
+//       </div>
+//     </div>
+//   )
+
+// }
+
+// export default App;
+
+import {useEffect, useState} from "react";
 
 function App(){
+  const [count, setCount] = useState(1);
 
-  const[post, setPost] = useState([]);
-
-  const postComponents = post.map((p, index) => <PostComponent
-    key={index}
-    name={p.name}
-    subtitle={p.subtitle}
-    time={p.time}
-    image={p.image}
-    description={p.description}
-  />)
-
-  // originally it was array of objects then we converted it to an array of objects and passed it into postComponents
-  //this is how to render a bunch of array of objects to seperate components
-
-  function addPost(){
-    setPost([...post, {
-      name: "aditya",
-      subtitle: "how to grow",
-      time: "2m ago",
-      image: "",
-      description: "howwwwwwww tooooooooo growwwwwww"
-    }
-
-    ])
+  function increment(){
+    setCount[c => c + 1];
   }
 
-  return (
-    <div style={{background: '#dfe6e9', height: "100vh"}}>
-      <button onClick={addPost}> ADD POST </button>
-      <div style={{display: "flex", justifyContent: "center"}}>
-        <div>
-          {postComponents}
-        </div>
-      </div>
-    </div>
-  )
+  useEffect( function() {
+    setInterval(increment, 1000);
+  }, [])
 
+  return <div>
+    {count}
+  </div>
 }
-
-export default App;
