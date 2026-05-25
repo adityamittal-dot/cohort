73e63d1e6631666210baec2a8d8d2b070bffc32c
adityamittal-dot <adityamittal529@gmail.com>
2026-05-25 14:34:51 +0530

    Add basic routing with react-router-dom for Landing and Class11th pages

--- SPA/SPA/src/App.jsx
+++ SPA/SPA/src/App.jsx
@@ -1,8 +1,28 @@
+import './App.css'
 import React from 'react';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+
 
 function App(){
   return <div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/neet/online-coaching-class-11" element={<Class11th/>}/>
+      <Route path ="/" element={<Landing/>}/>
+    </Routes>
+    </BrowserRouter>
 
+  </div>
+}
+function Landing(){
+  return <div>
+    this is the page where u land!
+  </div>
+}
+
+function Class11th(){
+  return <div>
+    this is for class 11th 
   </div>
 }
 
