eb8c08b4634599f930f13d3a8a93070dd4365b64
adityamittal-dot <adityamittal529@gmail.com>
2026-05-26 18:48:18 +0530

    fix: remove unused imports and fix typo atoms -> atom in App.jsx

--- SPA/SPA/src/App.jsx
+++ SPA/SPA/src/App.jsx
@@ -1,82 +1,97 @@
-import './App.css'
-import React from 'react';
-import {useState} from 'react'
-// import { BrowserRouter, Routes, Route} from 'react-router-dom';
+// import './App.css'
+// import React from 'react';
+// import {useState} from 'react'
+// // import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
+
+// // function App(){
+// //   return <div>
+// //     <BrowserRouter>
+// //     <Routes>
+// //       <Route path="/neet/online-coaching-class-11" element={<Class11th/>}/>
+// //       <Route path ="/" element={<Landing/>}/>
+// //     </Routes>
+// //     </BrowserRouter>
+
+// //   </div>
+// // }
+// // function Landing(){
+// //   return <div>
+// //     this is the page where u land!
+// //   </div>
+// // }
+
+// // function Class11th(){
+// //   return <div>
+// //     this is for class 11th 
+// //   </div>
+// // }
+
+// // export default App;
+// import {RecoilRoot, useRecoilValue, useSetRecoilState} from "recoil";
+// import { counterAtom } from './store/atoms/counter';
 
 // function App(){
+//   return (
+//     <RecoilRoot>
+//       <Counter/>
+//     </RecoilRoot>  
+//   ) 
+// }
+
+// function Counter(){
 //   return <div>
-//     <BrowserRouter>
-//     <Routes>
-//       <Route path="/neet/online-coaching-class-11" element={<Class11th/>}/>
-//       <Route path ="/" element={<Landing/>}/>
-//     </Routes>
-//     </BrowserRouter>
+//     <currentCount/>
+//     <Increment />
+//     <Decrement />
 
 //   </div>
 // }
-// function Landing(){
+
+// function currentCount(){
+
+//   const count = useRecoilValue(counterAtom);
+
 //   return <div>
-//     this is the page where u land!
+//     {count}
 //   </div>
 // }
 
-// function Class11th(){
+// function Increment(){
+
+//   const setCount = useSetRecoilState(counterAtom);
+//   function increase(){
+//     setCount(c=>c+1);
+//   }
 //   return <div>
-//     this is for class 11th 
+//     <button onClick={increase}>Increment</button>
 //   </div>
 // }
 
-// export default App;
-import {RecoilRoot, useRecoilValue, useSetRecoilState} from "recoil";
-import { counterAtom } from './store/atoms/counter';
+// function Decrement(){
+
+//   const setCount = useSetRecoilState(counterAtom);
+
+//   function decrease(){
+//     setCount(c => c-1);
+//   }
+//   return <div>
+//     <button onClick={decrease}>Decrement</button>
+//   </div>
+// }
+
+// export default App
+
+import {atom} from 'recoil';
 
 function App(){
-  return (
-    <RecoilRoot>
-      <Counter/>
-    </RecoilRoot>  
-  ) 
-}
-
-function Counter(){
   return <div>
-    <currentCount/>
-    <Increment />
-    <Decrement />
-
-  </div>
-}
-
-function currentCount(){
-
-  const count = useRecoilValue(counterAtom);
-
-  return <div>
-    {count}
-  </div>
-}
-
-function Increment(){
-
-  const setCount = useSetRecoilState(counterAtom);
-  function increase(){
-    setCount(c=>c+1);
-  }
-  return <div>
-    <button onClick={increase}>Increment</button>
-  </div>
-}
-
-function Decrement(){
-
-  const setCount = useSetRecoilState(counterAtom);
-
-  function decrease(){
-    setCount(c => c-1);
-  }
-  return <div>
-    <button onClick={decrease}>Decrement</button>
+    <button>home</button>
+    <button>MyNetwork ()</button>
+    <button>Job ()</button>
+    <button>Messaging ()</button>
+    <button>Notification ()</button>
+    <button>Me </button>
   </div>
 }
 
