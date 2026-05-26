149940f9e2b33a90f95d87059a19f2a2993d7f74
adityamittal-dot <adityamittal529@gmail.com>
2026-05-26 13:03:53 +0530

    Update App.jsx

--- SPA/SPA/src/App.jsx
+++ SPA/SPA/src/App.jsx
@@ -1,29 +1,69 @@
 import './App.css'
-import React from 'react';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+// import React from 'react';
+// import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
+
+// function App(){
+//   return <div>
+//     <BrowserRouter>
+//     <Routes>
+//       <Route path="/neet/online-coaching-class-11" element={<Class11th/>}/>
+//       <Route path ="/" element={<Landing/>}/>
+//     </Routes>
+//     </BrowserRouter>
+
+//   </div>
+// }
+// function Landing(){
+//   return <div>
+//     this is the page where u land!
+//   </div>
+// }
+
+// function Class11th(){
+//   return <div>
+//     this is for class 11th 
+//   </div>
+// }
+
+// export default App;
 
 function App(){
+  return (
+    <>
+      <Counter/>
+    </>
+  )
+}
+
+function Counter(){
+  const [count , setCount] = useState(0);
   return <div>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/neet/online-coaching-class-11" element={<Class11th/>}/>
-      <Route path ="/" element={<Landing/>}/>
-    </Routes>
-    </BrowserRouter>
+    {count}
+    <Increment setCount={setCount}/>
+    <Decrement setCount={setCount}/>
 
   </div>
 }
-function Landing(){
+
+function Increment({setCount}){
+
+  function increase(){
+    setCount(c=>c+1);
+  }
   return <div>
-    this is the page where u land!
+    <button onClick={increase}>Increment</button>
   </div>
 }
 
-function Class11th(){
+function Decrement({setCount}){
+
+  function decrease(){
+    setCount(c => c-1);
+  }
   return <div>
-    this is for class 11th 
+    <button onClick={decrease}>Decrement</button>
   </div>
 }
 
-export default App;
+export default App
