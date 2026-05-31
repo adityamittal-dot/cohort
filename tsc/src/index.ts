85ed51e934d4ada4699741bd47c5e8c39ce4763b
adityamittal-dot <adityamittal529@gmail.com>
2026-05-31 13:13:55 +0530

    Initialize modern TypeScript project in tsc folder

---
+++ tsc/src/index.ts
@@ -0,0 +1,25 @@
+// // Welcome to your TypeScript project!
+// // You can run this file in development using: npm run dev
+// // You can build this project using: npm run build
+
+// const greeting: string = "Hello, TypeScript with Node.js!";
+// console.log(greeting);
+
+// // Example of typed object
+// interface User {
+//   id: number;
+//   name: string;
+//   role: string;
+// }
+
+// const user: User = {
+//   id: 1,
+//   name: "Developer",
+//   role: "Admin"
+// };
+
+// console.log("Current user:", user);
+
+function greet(firstName: string){
+  console.log("hello" + firstName);
+}
